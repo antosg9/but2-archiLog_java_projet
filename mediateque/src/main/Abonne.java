f5dccd59d2e795cd69baaed0dc7edcd7afdfecd2
Kitsulfr <vidavidphanpro@gmail.com>
2024-05-22 10:50:40 +0200

    modification du file abonne

--- mediateque/src/main/Abonne.java
+++ mediateque/src/main/Abonne.java
@@ -1,5 +1,18 @@
 package main;
 
 public class Abonne {
-	//To-Do
+    private String nomAbonne;
+    private String prenomAbonne;
+    private int age;
+    private static int ageAdult = 18;
+
+    public Abonne (String nomAbonne, String prenomAbonne, int age){
+        this.nomAbonne = prenomAbonne;
+        this.prenomAbonne = nomAbonne;
+        this.age = age
+    }
+
+    public boolean isAdult (){
+        return this.age => ageAdult;
+    }
 }
